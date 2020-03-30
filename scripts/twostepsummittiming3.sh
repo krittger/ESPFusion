8ad0377877ffc3d926fdef24676e8a16b00c8de8
William Kleiber <william.kleiber@colorado.edu>
2020-03-30 08:05:34 -0600

    Adding more Summit timing tests
    Three more test days

---
+++ scripts/twostepsummittiming3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --qos=normal
+#SBATCH --account=ucb-general
+#SBATCH --ntasks-per-node=24
+#SBATCH --nodes=1
+#SBATCH --mem=100GB
+#SBATCH --time=160:00:00
+#SBATCH --output=twostep.summit.timing3.%j.out
+#SBATCH --job-name=twostep-timing3
+
+module purge
+
+module load R
+module load gdal
+module load proj
+
+R CMD BATCH --no-save --no-restore twostep.summit.timing.test3.R
