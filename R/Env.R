46c635493827e683c79958cebb8558baf6a499f2
M. J. Brodzik <brodzik@nsidc.org>
2020-04-28 14:19:27 -0600

    Moved generic prep function to package.

--- R/Env.R
+++ R/Env.R
@@ -8,14 +8,15 @@
 #'
 #' @return A list of directory locations used by the downscaling system
 #' @export
-Env <- function()
-{
+Env <- function() {
     me <- list(
         ## path to Landsat SSN files
         landsatDir = "/pl/active/SierraBighorn/scag/Landsat/UCSB_v3_processing/SSN/v01/",
         ## top level output directory, is assumed to contain subdirs:
         ## regression/, downscaled/, prob.btwn/ and prob.hundred/
-        fusionDir = "/pl/active/SierraBighorn/downscaledv3_test/",
+        fusionDir = "/pl/active/SierraBighorn/downscaledv3_test",
+        ## file with prepared setup information
+        setupFile = "/pl/active/SierraBighorn/Rdata/decade.setup.RData",
         ## 
         ## other shared information
         SSN30mRows = 14752,
@@ -27,5 +28,34 @@ Env <- function()
     class(me) <- append(class(me), "Env")
     return(me)
 }
+
+#' PrepOutDirs - creates required output directory structure
+#' 
+#' @export
+PrepOutDirs <- function(dirName, train.size) {
+    print(paste0("Prepping ", dirName, "..."))
+
+    ## Check/make output directories if they don't already exist
+    if (!(dir.exists(dirName))) {
+        print(paste0("Making new output directory: ", dirName))
+        dir.create(dirName, showWarnings=FALSE)
+    }
+
+    dlist <- c("downscaled", "prob.btwn", "prob.hundred", "regression")
+    outDirs <- list()
+    for (d in dlist) {
+        thisDir <- file.path(dirName, d, as.character(train.size))
+        if (!(dir.exists(thisDir))) {
+            print(paste0("Making new output directory: ",thisDir))
+            dir.create(thisDir, recursive=TRUE, showWarnings=FALSE)
+        }
+        outDirs[d] <- thisDir
+    }
+
+    print(paste0("...output directories OK"))
+
+    return(outDirs)
+    
+}    
 
 
