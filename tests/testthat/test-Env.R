1ebeb9daf076bdd823b69f40437bec48fc5d86db
M. J. Brodzik <brodzik@nsidc.org>
2020-06-23 09:54:01 -0600

    Fixed filenames for devtools conventions to work.

--- tests/testthat/test-Env.R
+++ tests/testthat/test-Env.R
@@ -1,13 +1,14 @@
 test_that("Env returns expected region information", {
 
     myEnv <- Env()
-    expect_equal(myEnv$regionShortName, "SSN")
+    expect_equal(myEnv$getRegionShortName(), "SSN")
 
 })
-test_that("Env returns good MODIS filenames", {
+test_that("Env returns good v01 MODIS filenames", {
 
     myEnv <- Env()
-    fileName <- myEnv$modisFileFor(2004, 32);
+    fileName <- myEnv$modisFileFor(1, 2004, 32, "snow_cover_percent")
+    print(fileName)
     expect_equal(fileName,
                  paste0("/pl/active/SierraBighorn/scag/MODIS/SSN/v01/",
                         "SSN.SN_WY2004_20040201.Terra-MODIS.snow_cover_percent.v01.tif"))
