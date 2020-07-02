38b1fc5aba059e94cbfea265cf2bb3227a5bc200
ShaliniMa <shalini.mahanthege@colorado.edu>
2020-07-02 16:00:12 -0600

    Final changes to growforest.R

--- exec/twostep.growforest.R
+++ exec/twostep.growforest.R
@@ -1,20 +1,13 @@
-#specify the number of points to be randomly sampled from the good indices each day. 
-
 
 library(fields)
 library(raster)
 library(ranger)
 
-myEnv <- Env()
-SSN <- StudyExtent("SouthernSierraNevada")
+myEnv <- ESPFusion::Env()
+studyExtent <- ESPFusion::StudyExtent("SouthernSierraNevada")
 
 
 ## List data files and set up region bounds
-
-clear.landsat.path <- "/pl/active/SierraBighorn/scag/Landsat/UCSB_v3_processing/SSN/v01/"
-cloudy.landsat.path <- "/pl/active/SierraBighorn/scag/Landsat/UCSB_v3_processing/SSN_cloudy/v01/"
-Rdata_path <- "/pl/active/SierraBighorn/Rdata/"
-
 modis.sc.file.names <- myEnv$allModisFiles("snow_cover_percent",version=3)
 
 clear.sat.mask.file.names <- myEnv$allLandsatFiles("saturation_mask",version=1,includeCloudy=FALSE)
@@ -29,8 +22,6 @@ sat.mask.file.names <- c(clear.sat.mask.file.names,cloudy.sat.mask.file.names)
 rm(cloudy.sat.mask.file.names,clear.sat.mask.file.names)
 
 ## Get dates
-### CHECK: make sure the substr on the new modis.sc.file.names actually grabs yyyymmdd
-####changed start and end to reflect date position
 mod.date <- lst.date <- cloudy.lst.date <- NULL
 for(i in 1:length(modis.sc.file.names)){
 	mod.date[i] <- regmatches(modis.sc.file.names[i], regexpr("\\d{8}",modis.sc.file.names[i]))
@@ -58,7 +49,6 @@ lst.date <- strptime(x=as.character(lst.date),format="%Y%m%d")
 out <- format(mod.date,"%Y%m%d") %in% format(lst.date,"%Y%m%d")
 modis.sc.file.names <- modis.sc.file.names[out]
 mod.date <- mod.date[out]
-
 
 
 out <- format(lst.date,"%Y%m%d") %in% format(mod.date,"%Y%m%d")
@@ -95,63 +85,63 @@ nday <- length(mod.da)
 ##
 
 out <- raster("/pl/active/SierraBighorn/predictors/SouthernSierraNevada_Elevation.tif")
-elev <- t(matrix(values(out),nc=SSN$highResRows,nr=SSN$highResCols))
+elev <- t(matrix(values(out),nc=studyExtent$highResRows,nr=studyExtent$highResCols))
 elev[elev < (-100)] <- NA
 rm(out)
 
 out <- raster("/pl/active/SierraBighorn/predictors/SouthernSierraNevada_Slope.tif")
-slope <- t(matrix(values(out),nc=SSN$highResRows,nr=SSN$highResCols))
+slope <- t(matrix(values(out),nc=studyExtent$highResRows,nr=studyExtent$highResCols))
 slope[slope < 0] <- NA
 rm(out)
 
 out <- raster("/pl/active/SierraBighorn/predictors/SouthernSierraNevada_Aspect.tif")
-asp <- t(matrix(values(out),nc=SSN$highResRows,nr=SSN$highResCols))
+asp <- t(matrix(values(out),nc=studyExtent$highResRows,nr=studyExtent$highResCols))
 asp[asp < (-2)] <- NA
 rm(out)
 
 out <- raster("/pl/active/SierraBighorn/predictors/SouthernSierraNevada_LandClassNLCD.tif")
-lty <- t(matrix(values(out),nc=SSN$highResRows,nr=SSN$highResCols))
+lty <- t(matrix(values(out),nc=studyExtent$highResRows,nr=studyExtent$highResCols))
 rm(out)
 
 out <- raster("/pl/active/SierraBighorn/landcover/LandFireEVH_ucsb/SSN.LandFireEVH_SN30m_height_m.v01.tif")
-forest.height <- t(matrix(values(out),nc=SSN$highResRows,nr=SSN$highResCols))
+forest.height <- t(matrix(values(out),nc=studyExtent$highResRows,nr=studyExtent$highResCols))
 rm(out)
 
 out <- raster("/pl/active/SierraBighorn/predictors/SouthernSierraNevada_NorthWestBarrierDistance.tif")
-nw.barrierdist <- t(matrix(values(out),nc=SSN$highResRows,nr=SSN$highResCols))
+nw.barrierdist <- t(matrix(values(out),nc=studyExtent$highResRows,nr=studyExtent$highResCols))
 rm(out)
 
 out <- raster("/pl/active/SierraBighorn/predictors/SouthernSierraNevada_SouthWestBarrierDistance.tif")
-sw.barrierdist <- t(matrix(values(out),nc=SSN$highResRows,nr=SSN$highResCols))
+sw.barrierdist <- t(matrix(values(out),nc=studyExtent$highResRows,nr=studyExtent$highResCols))
 rm(out)
 
 out <- raster("/pl/active/SierraBighorn/predictors/SouthernSierraNevada_WestBarrierDistance.tif")
-w.barrierdist <- t(matrix(values(out),nc=SSN$highResRows,nr=SSN$highResCols))
+w.barrierdist <- t(matrix(values(out),nc=studyExtent$highResRows,nr=studyExtent$highResCols))
 rm(out)
 
 out <- raster("/pl/active/SierraBighorn/predictors/SouthernSierraNevada_SouthWestDistanceToWater.tif")
-sw.waterdist <- t(matrix(values(out),nc=SSN$highResRows,nr=SSN$highResCols))
+sw.waterdist <- t(matrix(values(out),nc=studyExtent$highResRows,nr=studyExtent$highResCols))
 rm(out)
 
 out <- raster("/pl/active/SierraBighorn/predictors/SouthernSierraNevada_WestDistanceToWater.tif")
-w.waterdist <- t(matrix(values(out),nc=SSN$highResRows,nr=SSN$highResCols))
+w.waterdist <- t(matrix(values(out),nc=studyExtent$highResRows,nr=studyExtent$highResCols))
 rm(out)
 
 out <- raster("/pl/active/SierraBighorn/predictors/downscaled_s_sierra_winds_dec_april_climatology_nldas2.tif")
-windspeed <- t(matrix(values(out),nc=SSN$highResRows,nr=SSN$highResCols))
+windspeed <- t(matrix(values(out),nc=studyExtent$highResRows,nr=studyExtent$highResCols))
 rm(out)
 
 
-locs <- expand.grid(x=1:SSN$highResCols,y=1:SSN$highResRows) #expand.grid(x=seq(from=129187.3,by=30,length.out= SSN$highResCols), y=seq(from=3918807,by=30,length.out= SSN$highResRows))
-locs.x <-  t(matrix(locs[,1],nc= SSN$highResRows,nr= SSN$highResCols))
-locs.y <-  t(matrix(locs[,2],nc= SSN$highResRows,nr= SSN$highResCols))
-locs <- cbind(c(locs.x),rev(locs.y))/SSN$highResRows
+locs <- expand.grid(x=1:studyExtent$highResCols,y=1:studyExtent$highResRows) #expand.grid(x=seq(from=129187.3,by=30,length.out= studyExtent$highResCols), y=seq(from=3918807,by=30,length.out= studyExtent$highResRows))
+locs.x <-  t(matrix(locs[,1],nc= studyExtent$highResRows,nr= studyExtent$highResCols))
+locs.y <-  t(matrix(locs[,2],nc= studyExtent$highResRows,nr= studyExtent$highResCols))
+locs <- cbind(c(locs.x),rev(locs.y))/studyExtent$highResRows
 
 
-smalllocs <- expand.grid(x=1:SSN$lowResCols,y=1:SSN$lowResRows) #expand.grid(x=seq(from=129187.3,to= 420517.3,length.out= SSN$lowResCols), y=seq(from=3918807,to= 4361337,length.out= SSN$lowResRows))
-smalllocs.x <-  t(matrix(smalllocs[,1],nc= SSN$lowResRows,nr= SSN$lowResCols))
-smalllocs.y <-  t(matrix(smalllocs[,2],nc= SSN$lowResRows,nr= SSN$lowResCols))
-smalllocs <- cbind(c(smalllocs.x),rev(smalllocs.y))/SSN$lowResRows
+smalllocs <- expand.grid(x=1:studyExtent$lowResCols,y=1:studyExtent$lowResRows) #expand.grid(x=seq(from=129187.3,to= 420517.3,length.out= studyExtent$lowResCols), y=seq(from=3918807,to= 4361337,length.out= studyExtent$lowResRows))
+smalllocs.x <-  t(matrix(smalllocs[,1],nc= studyExtent$lowResRows,nr= studyExtent$lowResCols))
+smalllocs.y <-  t(matrix(smalllocs[,2],nc= studyExtent$lowResRows,nr= studyExtent$lowResCols))
+smalllocs <- cbind(c(smalllocs.x),rev(smalllocs.y))/studyExtent$lowResRows
 
 #plot with quilt.plot(locs,c(elev))
 
@@ -180,22 +170,20 @@ feature.NA <- which(is.na(elev))
 
 for(day in 1:nday){ 
 	
-	LST  <- t(matrix(values(raster(landsat.sc.file.names[day])),nc= SSN$highResRows,nr= SSN$highResCols))
+	LST  <- t(matrix(values(raster(landsat.sc.file.names[day])),nc= studyExtent$highResRows,nr= studyExtent$highResCols))
 	LST.NA <- which(LST==255)
 	
-	sat.mask <- t(matrix(values(raster(sat.mask.file.names[day])),nc= SSN$highResRows,nr= SSN$highResCols))
+	sat.mask <- t(matrix(values(raster(sat.mask.file.names[day])),nc= studyExtent$highResRows,nr= studyExtent$highResCols))
 	LST.sat <- which(sat.mask==1)
 	rm(sat.mask)
 	
-	these.good.day <- (1:(SSN$highResRows*SSN$highResCols))[-c(LST.sat,LST.NA,feature.NA)]
+	these.good.day <- (1:(studyExtent$highResRows*studyExtent$highResCols))[-c(LST.sat,LST.NA,feature.NA)]
 	train.indices.day <- sample(these.good.day,myEnv$getTrain.size())
 	
 	train.LST[[day]] <- LST[train.indices.day]
 	rm(LST)
-### might need to be careful here since this only calls a single modis.path	
-####added year inside path
-	MOD.big <- t(matrix(values(raster(modis.sc.file.names[day])),nc=SSN$highResRows,nr=SSN$highResCols))
-###
+
+	MOD.big <- t(matrix(values(raster(modis.sc.file.names[day])),nc=studyExtent$highResRows,nr=studyExtent$highResCols))
 
   train.MOD[[day]] <- MOD.big[train.indices.day]
   rm(MOD.big)
@@ -218,12 +206,7 @@ for(day in 1:nday){
 	print(paste0("Day ", day, " of ", nday, ", ", length(train.indices.day), " training indices from ", length(these.good.day), " possible"))
 }
 
-### I think this is unnecessary
-train.da.nNA <- unlist(lapply(train.LST,length))
-sum(train.da.nNA)/1e6 # how many millions of data points you grabbed
-### to here
 #training data fully constructed here
-
 train.dat <- data.frame(lst=unlist(train.LST),
 	da=rep(mod.da,times=train.da.nNA),
 	elev=unlist(train.elev),
@@ -270,12 +253,10 @@ print(Sys.time())
 print(object.size(ranger.classifier),units="GB")
 ranger.classifier$prediction.error
 
-### Need to change the filename to reflect v02
 print(Sys.time())
 save(ranger.classifier,file=myEnv$getModelFilenameFor("classifier",version=3))
 print(Sys.time())
 rm(ranger.classifier)
-###
 
 ##
 ## Growing and saving the regression forest
@@ -290,8 +271,7 @@ print(Sys.time())
 print(object.size(ranger.regression),units="GB")
 ranger.regression$prediction.error
 
-### same thing: need to change filename to reflect v02
-save(ranger.regression,file=file=myEnv$getModelFilenameFor("regression",version=3))
-###
+save(ranger.regression,file=myEnv$getModelFilenameFor("regression",version=3))
+
 
 
