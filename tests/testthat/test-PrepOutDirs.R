1a9a973744593c7f687d804973decf074edbff1e
M. J. Brodzik <brodzik@nsidc.org>
2020-08-16 16:21:51 -0600

    Added year to output directory paths.
    To avoid indefinitely writing new data to the same directories.
    Advice from RC is to keep number of files in any one directory < 1000.

--- tests/testthat/test-PrepOutDirs.R
+++ tests/testthat/test-PrepOutDirs.R
@@ -4,14 +4,15 @@ test_that("PrepOutDirs returns expected directories", {
     ## that includes this file
     dir <- './testOut'
     tsize <- 3e+05
-    outDirs <- PrepOutDirs(dir, tsize)
+    year <- 2001
+    outDirs <- PrepOutDirs(dir, tsize, year)
 
     dlist <- c("downscaled", "prob.btwn", "prob.hundred", "regression")
     expect_equal(length(outDirs), length(dlist))
     for (d in dlist) {
         expect_equal(outDirs[[d]],
-                     sprintf("%s/%s/%s",
-                             dir, d, as.character(tsize)))
+                     sprintf("%s/%s/%s/%4d",
+                             dir, d, as.character(tsize), year))
     }
 
     # Cleanup
